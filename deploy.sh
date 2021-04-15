b496e3902dbd79038d4eeada4623610b6278385e
hitzhangjie <hit.zhangjie@gmail.com>
2021-04-15 11:26:22 +0800

    添加部署脚本

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+publishDir=hitzhangjie.github.io
+
+rm $publishDir/index.min.*js
+rm $publishDir/main.*.css
+rm $publishDir/main.min.*.js
+
+npm run build
+
+cd $publishDir
+msg="$(date +'%F %T') rebuild site"
+git add .
+git cc -m "$msg"
+git push
